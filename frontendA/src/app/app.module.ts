d1dc3dccb259aab2f5a848c7a403a618cc0dedf2
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-08-30 21:11:20 -0600

    pantalla login - marcaje y admin
    implementacion de navbar con login, marcaje y admin (base)

--- frontendA/src/app/app.module.ts
+++ frontendA/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MarcajeComponent } from './marcaje/marcaje.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MarcajeComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
