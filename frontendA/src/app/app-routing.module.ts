a1c39933f020cf0e836c5d3b6b4c0a6aea8e6a02
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-08-30 21:11:20 -0600

    pantalla login - marcaje y admin
    implementacion de navbar con login, marcaje y admin (base)

--- frontendA/src/app/app-routing.module.ts
+++ frontendA/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { MarcajeComponent } from './marcaje/marcaje.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'marcaje', component: MarcajeComponent },
+  { path: 'admin', component: AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontendA/src/app/marcaje/marcaje.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marcaje',
+  templateUrl: './marcaje.component.html',
+  styleUrls: ['./marcaje.component.css']
+})
+export class MarcajeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
